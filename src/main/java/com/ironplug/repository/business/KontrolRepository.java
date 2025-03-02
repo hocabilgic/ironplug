fb19e3580a929ad1ca241b5317db317886a2290b
Emre AKTAŞ <132508366+aktasemre@users.noreply.github.com>
2025-03-02 21:24:18 +0300

    kontrole image eklendi , kontrol nesnesi 7 gun sonra siliniyor , baslik artik  null donmuyor title varsa update ediyor yoksa olusturuyor

--- src/main/java/com/ironplug/repository/business/KontrolRepository.java
+++ src/main/java/com/ironplug/repository/business/KontrolRepository.java
@@ -4,5 +4,19 @@ import com.ironplug.entity.business.Image;
 import com.ironplug.entity.business.Kontrol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface KontrolRepository extends JpaRepository<Kontrol, Long> {
+
+
+    // 7 günden eski kontrolleri bulan sorgu
+    List<Kontrol> findByCreateAtBefore(ZonedDateTime dateTime);
+
+    // 7 günden eski kontrolleri silen metod
+    void deleteByCreateAtBefore(ZonedDateTime dateTime);
+
+    Optional<Kontrol> findByBaslikId(Long baslikId);
+
 }
